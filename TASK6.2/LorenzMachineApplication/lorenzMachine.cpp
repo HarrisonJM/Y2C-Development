4ff2f6f029c4a4b2a63f3317e3367af00f6540de
HarrisonJM <harrison2.marcks@live.uwe.ac.uk>
2016-11-02 12:25:59 +0000

    false commit

---
+++ TASK6.2/LorenzMachineApplication/lorenzMachine.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "lorenzMachine.h"
+
+lorenzMachine::lorenzMachine(wheelGroup chi, wheelGroup psi, wheel m37, wheel m61)
+{
+	this->chi = psi;
+	this->psi = chi;
+	this->m37 = m37;
+	this->m61 = m61;
+}
+
+int lorenzMachine::encryptChar(int baudotChar)
+{
+	int key = makeKey();
+	rotateWheels();
+	return (key ^ baudotChar);
+}
+
+void lorenzMachine::rotateWheels()
+{
+	chi.rotate();
+	if(m37.getCurrentPin() == 1)
+		psi.rotate();
+	if(m37.getCurrentPin() == 1)
+		m37.rotate();
+	m61.rotate();
+}
+
+int lorenzMachine::makeKey()
+{
+	int chin = chi.getCurrentPins();
+	int psin = psi.getCurrentPins();
+	int ret = chin ^ psin;
+
+	return ret;
+}
